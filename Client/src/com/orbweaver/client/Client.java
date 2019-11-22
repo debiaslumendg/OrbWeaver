287b189594387369fea29ba3f8663c6a8fb32643
Naty-S <na7a5cha010@gmail.com>
2019-11-22 12:50:50 -0400

    Client class. Servers name and port

---
+++ Client/src/com/orbweaver/client/Client.java
@@ -0,0 +1,9 @@
+package com.orbweaver.client;
+
+public class Client {
+
+    // linked list con servidor y puerto de cada servidor
+    // array list de todos los servidores disponibles y el scheduler es uno de ellos
+
+    public Client() {}
+}
